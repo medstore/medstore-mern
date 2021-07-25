3f8252c3ea7cf4cbe44f08507a2a06fa40e1e7fb
Sushil kumar Gupta <skgupta77159@gmail.com>
2021-07-25 20:59:44 +0530

    Added home, signin, signup page

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+
+import Topbar from './components/topbar/Topbar';
+import Home from './pages/home/Home';
+import Signin from './pages/signin/Signin';
+import Signup from './pages/signup/Signup';
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello World </h1>
-      <h2>web application </h2>
+      <BrowserRouter>
+        <Topbar />
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/signin" component={Signin} />
+          <Route exact path="/signup" component={Signup} />
+        </Switch>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ client/src/pages/signin/Signin.jsx
@@ -0,0 +1,10 @@
+import './signin.css'
+
+export default function Signin(){
+    return(
+        <>
+        <h1>SignIn Page</h1>
+        <p>Happy Development :)</p>
+        </>
+    )
+}
