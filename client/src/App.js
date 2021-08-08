251e2fef3af4e435644fcd72da2ce6c446ad8130
Sushil kumar Gupta <skgupta77159@gmail.com>
2021-08-08 13:24:50 +0530

    added edit detail form

--- client/src/App.js
+++ client/src/App.js
@@ -6,17 +6,22 @@ import Topbar from './components/topbar/Topbar';
 import Home from './pages/home/Home';
 import Signin from './pages/signin/Signin';
 import Signup from './pages/signup/Signup';
+import Profile from './pages/profile/Profile';
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
         <Topbar />
-        <Switch>
-          <Route exact path="/" component={Home} />
-          <Route exact path="/signin" component={Signin} />
-          <Route exact path="/signup" component={Signup} />
-        </Switch>
+        <div className="appWrapper">
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route exact path="/signin" component={Signin} />
+            <Route exact path="/signup" component={Signup} />
+            <Route exact path="/profile" component={Profile} />
+          </Switch>
+        </div>
+
       </BrowserRouter>
     </div>
   );
