707c59d33ba7fc658474ef29dbcfb339d5d23bf1
Sushil kumar Gupta <skgupta77159@gmail.com>
2021-08-12 20:32:57 +0530

    added profile and cart badge on tapbar

--- client/src/components/topbar/Topbar.jsx
+++ client/src/components/topbar/Topbar.jsx
@@ -1,9 +1,10 @@
 import './topbar.css'
 import { NavLink } from 'react-router-dom';
 import { useState } from 'react';
+import emptyprofile from "../../images/emptyprofile.png"
 
 export default function Topbar() {
-    const [user, setUser] = useState(false);
+    const [user, setUser] = useState(true);
     return (
         <div className="topbar">
             <div className="topbarWrapper">
@@ -13,7 +14,7 @@ export default function Topbar() {
                         <NavLink exact className="nav-link" to="/">Home</NavLink>
                     </li>
                     {
-                        user ? null :
+                        !user ? null :
                             <>
                                 <li className="nav-item">
                                     <NavLink exact className="nav-link" to="/signin">Login</NavLink>
@@ -24,6 +25,18 @@ export default function Topbar() {
                             </>
                     }
                 </ul>
+                {
+                    user ? 
+                    <div className="topbarProfile">
+                            <img className="topbarProfImg" src={emptyprofile} />
+                            <span className="topbarEmail">abcd@gmail.com</span>
+                            <div className="cartDiv">
+                                <span className="cartNumber">15</span>
+                                <i class="fa fa-shopping-cart" aria-hidden="true"></i>
+                            </div>
+                            
+                        </div> : null
+                }
             </div>
         </div>
     )
