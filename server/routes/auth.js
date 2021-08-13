12c56706e3b8102ba578ecfedce1a4fd038273cb
AkashYadav-02 <akashydv2212@gmail.com>
2021-08-13 11:07:20 +0530

    server changes done

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -2,7 +2,14 @@ const express = require('express');
 const router = express.Router();
 const {signin, signup} = require('../controllers/auth')
  
-router.route("/signin").post(signin);
+
+
+
+
 router.route("/signup").post(signup);
+
+router.route("/signin").post(signin);
+
+
 
 module.exports = router;
