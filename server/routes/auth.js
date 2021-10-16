c2fb4f7230f6098987df35db9e82498bf96235bd
Sushil kumar Gupta <skgupta77159@gmail.com>
2021-10-16 20:39:53 +0530

    update user profile backend completed

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const router = express.Router();
-const {signin, signup} = require('../controllers/auth')
+const {signin, signup, updateuser} = require('../controllers/auth')
 
 router.route("/signup").post(signup);
 router.route("/signin").post(signin);
+router.route("/updateuserdata").post(updateuser);
 
 module.exports = router;
